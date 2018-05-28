cbb21f746a1d2385453d8d0e4eeeea3dcf968594
yangxb <yangxb@gridsatellite.com>
2018-05-28 11:14:22 +0800

    first

---
+++ src/main/java/com/gxw/Main.java
@@ -0,0 +1,11 @@
+package com.gxw;
+
+import javafx.application.Application;
+
+public class Main{
+    public static void main(String[] agrs){
+        System.out.println("Hello World!");
+    }
+
+
+} 
